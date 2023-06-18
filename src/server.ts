ff2bfabd0e94955a2f258dec16de550134ce06a3
Reijki007 <rodrigo_s.sousa@outlook.com.br>
2023-06-17 23:50:23 -0300

    Criando o servidor - pré-configurado

---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import express from "express";
+import dotenv from 'dotenv';
+import mustache from 'mustache-express';
+import path from 'path';
+
+dotenv.config();
+
+const server = express();
+
+server.set('view engine', 'mustache');
+server.set(__dirname, 'views');
+server.engine('mustache', mustache());
+
+server.use(express.static(path.join(__dirname, '../public')));
+
+
+
+
+server.listen(process.env.PORT);
