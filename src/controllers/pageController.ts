fd3206de403f6d866712c79121ec62700ac2c8af
Reijki007 <rodrigo_s.sousa@outlook.com.br>
2023-06-19 02:25:39 -0300

    Criando o Model Pet

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,6 +1,6 @@
 import {Request, Response} from 'express';
 import {createMenuObject} from '../helpers/createMenuObject';
-import { create } from 'domain';
+
 
 export const home = (req:Request, res:Response) => {        
     res.render('pages/page', {
